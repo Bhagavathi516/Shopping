fd7dd6ff12ba588fcb446a13fb76d99c9a8a7a93
Bhagavathi <kottesomanadhabhagavathi@gmail.com>
2023-08-09 12:37:41 +0530

    added base template

---
+++ shopping/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import shoppingItem
+# Register your models here.
+admin.site.register(shoppingItem)
---
+++ shopping/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+class shoppingItem(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)] )
+    discount = models.FloatField(null=True, blank=True, default=0.0 )
+    description = models.TextField()
+    
+    def __str__(self) :
+        return self.name
